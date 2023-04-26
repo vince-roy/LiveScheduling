83e63d75847e58ef2cd331fadd8dee211da8d15c
vince roy <vince@potionapps.com>
2023-04-26 09:20:08 -0400

    format

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule LiveScheduling.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:phoenix, "~> 1.7.2"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
--- test/live_scheduling_web/controllers/error_json_test.exs
+++ test/live_scheduling_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule LiveSchedulingWeb.ErrorJSONTest do
   use LiveSchedulingWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert LiveSchedulingWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert LiveSchedulingWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do
