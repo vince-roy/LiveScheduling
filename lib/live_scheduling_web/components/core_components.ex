351e67ce15136d48d2eaad7e741e2c14d58e5398
vince roy <vince@potionapps.com>
2023-06-04 21:31:53 -0400

    core component tweak

--- lib/live_scheduling_web/components/core_components.ex
+++ lib/live_scheduling_web/components/core_components.ex
@@ -113,19 +113,25 @@ defmodule LiveSchedulingWeb.CoreComponents do
       phx-click={JS.push("lv:clear-flash", value: %{key: @kind}) |> hide("##{@id}")}
       role="alert"
       class={[
-        "fixed top-2 right-2 w-80 sm:w-96 z-50 rounded-lg p-3 ring-1",
+        "flex fixed top-3 left-1/2 -translate-x-1/2 max-w-[95vw] z-50 rounded-lg ring-1",
+        "px-2",
+        "py-1",
+        "items-center",
         @kind == :info && "bg-emerald-50 text-emerald-800 ring-emerald-500 fill-cyan-900",
         @kind == :error && "bg-rose-50 text-rose-900 shadow-md ring-rose-500 fill-rose-900"
       ]}
       {@rest}
     >
       <p :if={@title} class="flex items-center gap-1.5 text-sm font-semibold leading-6">
-        <.icon :if={@kind == :info} name="hero-information-circle-mini" class="h-4 w-4" />
-        <.icon :if={@kind == :error} name="hero-exclamation-circle-mini" class="h-4 w-4" />
+        <.icon :if={@kind == :info} name="hero-bell" class="h-4 w-4" />
+        <.icon :if={@kind == :error} name="hero-exclamation-circle" class="h-4 w-4" />
         <%= @title %>
       </p>
-      <p class="mt-2 text-sm leading-5"><%= msg %></p>
-      <button type="button" class="group absolute top-1 right-1 p-2" aria-label={gettext("close")}>
+      <svg viewBox="0 0 2 2" class="mx-2 inline h-1 w-1 fill-current" aria-hidden="true">
+        <circle cx="1" cy="1" r="1" />
+      </svg>
+      <p class="text-sm leading-5"><%= msg %></p>
+      <button type="button" class="flex group p-2" aria-label={gettext("close")}>
         <.icon name="hero-x-mark-solid" class="h-5 w-5 opacity-40 group-hover:opacity-70" />
       </button>
     </div>
